9699e5bdbb6b6601e0a494cf82a23fb0efc208b7
oni128 <124542644+oni128@users.noreply.github.com>
2025-01-25 23:49:43 +0900

    [level 0] Title: 배열 만들기 1, Time: 7.40 ms, Memory: 89.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i % k == 0){
+                arr.add(i);
+            }
+        }
+        int[] answer = new int[arr.size()];
+        for(int i=0; i<arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+            
+            
+        return answer;
+    }
+}
