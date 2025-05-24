4d72d1acf22cedc2d4cf22020864c0a41e2a0601
oni128 <lyw283828@gmail.com>
2025-05-24 15:23:34 +0900

    Create 숨어있는 숫자의 덧셈(1) -5

---
+++ 프로그래머스/0/숨어있는 숫자의 덧셈 (1)/숨어있는 숫자의 덧셈 (1) - 5.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str = my_string.replaceAll("[^0-9]", "");
+        for(char i : str.toCharArray()) {
+            answer += Character.getNumericValue(i);
+        }
+        return answer;
+    }
+}
